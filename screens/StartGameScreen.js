29a2e9655d5a814a5edd294761b3dd9a8fc117c5
VinothKumar <89235649+vinowoualabs@users.noreply.github.com>
2022-02-21 12:26:50 +0530

    Styles a View as a Card Container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -27,6 +27,14 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
+    shadowColor: 'black',
+    /* shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: '#fff',*/ //Works only In IOS
+    elevation: 5,
+    padding: 25,
+    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
